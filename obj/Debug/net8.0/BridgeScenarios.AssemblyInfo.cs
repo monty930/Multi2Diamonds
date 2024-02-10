843b6366beae07e956b701de3fa92e2c372d6780
Krystyna Gasinska <krystyna.gas01@gmail.com>
2024-02-10 23:00:51 +0100

    line position preserved

--- obj/Debug/net8.0/BridgeScenarios.AssemblyInfo.cs
+++ obj/Debug/net8.0/BridgeScenarios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BridgeScenarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ccba42d2e8a4fa7e6b5c18198783b01fb829eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0545a0d237599862684d8c5d30fd6699a7bbaea2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BridgeScenarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BridgeScenarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
