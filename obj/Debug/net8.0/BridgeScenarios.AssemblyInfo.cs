0545a0d237599862684d8c5d30fd6699a7bbaea2
Krystyna Gasinska <krystyna.gas01@gmail.com>
2024-02-10 22:54:25 +0100

    line numbers fixed

--- obj/Debug/net8.0/BridgeScenarios.AssemblyInfo.cs
+++ obj/Debug/net8.0/BridgeScenarios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BridgeScenarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f212caddfc963dcb242e07434c3cadde18aeb01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ccba42d2e8a4fa7e6b5c18198783b01fb829eeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BridgeScenarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BridgeScenarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
