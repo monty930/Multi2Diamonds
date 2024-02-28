ec42d049580a613781d65caa9b05c0d1b2cfa57f
bartn1k <bartek.slupik@gmail.com>
2024-02-28 12:57:39 +0100

    Shell script for windows

--- Chai/get_scenarios.sh
+++ Chai/get_scenarios.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname "$0")
+CHAI="$SCRIPT_DIR/$1"
+TEMPFILE=$(mktemp $SCRIPT_DIR/chai_output.XXXXXX)
+FILEPATH=$3
+N_DEALS=$4
+
 
 for file in "$SCRIPT_DIR"/grammar/Bridgechai/*.hs; do
     if [[ $(basename "$file") != "Test.hs" ]]; then
@@ -14,30 +19,21 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-FILEPATH=$1
-
-TEMPFILE=$(mktemp /tmp/chai_output.XXXXXX)
-
-output=$("$SCRIPT_DIR/chai_py" "$FILEPATH" 2>&1)
-
+CHAI_OUTPUT=$("$CHAI" "$FILEPATH" 2>&1)
 if [ $? -ne 0 ]; then
     echo "Execution of chai_py failed."
-    echo "$output"
+    echo "$CHAI_OUTPUT"
     exit 1
 fi
+echo "$CHAI_OUTPUT" > "$TEMPFILE"
 
-echo "$output" > "$TEMPFILE"
 
-num_of_deals=$2
-
-output2=$(python -mredeal --max 100000 -n $num_of_deals --format=pbn "$TEMPFILE" 2>&1)
-
+REDEAL_OUTPUT=$(python -mredeal --max 100000 -n "$N_DEALS" --format=pbn "$TEMPFILE" 2>&1)
 if [ $? -ne 0 ]; then
     echo "Execution of redeal failed."
-    echo "$output2"
+    echo "$REDEAL_OUTPUT"
     exit 1
 fi
-
-echo "$output2"
+echo "$REDEAL_OUTPUT"
 
 rm "$TEMPFILE"
