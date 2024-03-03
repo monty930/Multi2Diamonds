932f05d81305b07fbc79a91b253ce136ca6ff879
bartn1k <bartek.slupik@gmail.com>
2024-03-03 16:29:52 +0100

    Migrations

---
+++ Migrations/20240303152428_Initial.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BridgeScenarios.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<byte[]>(type: "bytea", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    IsAdmin = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DealSets",
+                columns: table => new
+                {
+                    DealSetId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DealSets", x => x.DealSetId);
+                    table.ForeignKey(
+                        name: "FK_DealSets_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Deals",
+                columns: table => new
+                {
+                    DbDealId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DealSetId = table.Column<int>(type: "integer", nullable: false),
+                    Dealer = table.Column<int>(type: "integer", nullable: false),
+                    Number = table.Column<int>(type: "integer", nullable: false),
+                    Vul = table.Column<int>(type: "integer", nullable: false),
+                    West = table.Column<string>(type: "text", nullable: false),
+                    North = table.Column<string>(type: "text", nullable: false),
+                    East = table.Column<string>(type: "text", nullable: false),
+                    South = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deals", x => x.DbDealId);
+                    table.ForeignKey(
+                        name: "FK_Deals_DealSets_DealSetId",
+                        column: x => x.DealSetId,
+                        principalTable: "DealSets",
+                        principalColumn: "DealSetId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deals_DealSetId",
+                table: "Deals",
+                column: "DealSetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DealSets_UserId",
+                table: "DealSets",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Deals");
+
+            migrationBuilder.DropTable(
+                name: "DealSets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
