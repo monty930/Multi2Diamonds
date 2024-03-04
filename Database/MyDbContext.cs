f1378b9ab5b0d2dc8aa8ece8e9ee6463b129bb91
bartn1k <bartek.slupik@gmail.com>
2024-03-04 16:07:57 +0100

    RemoveHandModel Migration

--- Database/MyDbContext.cs
+++ Database/MyDbContext.cs
@@ -5,7 +5,7 @@ namespace BridgeScenarios.Database;
 
 public class MyDbContext : DbContext
 {
-    public DbSet<User?> Users { get; set; }
+    public DbSet<User> Users { get; set; }
     public DbSet<DealSet> DealSets { get; set; }
     public DbSet<Deal> Deals { get; set; }
 
--- Models/DbModels/DealSet.cs
+++ Models/DbModels/DealSet.cs
@@ -1,12 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BridgeScenarios.Models.DbModels;
 
 public class DealSet
 {
+    [Key]
     public int DealSetId { get; set; }
     
+    [ForeignKey("User")]
     public int UserId { get; set; }
     public User User { get; set; } = null!;
     
+    [StringLength(40)]
     public string Name { get; set; } = string.Empty;
     
     public List<Deal> Deals { get; set; } = [];
