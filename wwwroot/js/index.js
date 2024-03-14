5ba305fbb002ac9b60d11cc3dad817d95e67e2e8
Krystyna Gasinska <krystyna.gas01@gmail.com>
2024-03-14 20:37:49 +0100

    divider and small window view

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -7,84 +7,69 @@ document.addEventListener('DOMContentLoaded', function() {
     let isDragging = false;
     let leftWidthRatio = 0.5;
     let rightDependentRatio = 0.035;
+    let minWidth = 400;
     let rightPanel = document.getElementById('right-partial');
     let leftPanel = document.getElementById('left-partial');
+    let smallView = false;
+    const leftSidePaddingLayout = document.getElementById('left-side-height');
 
     function updateSidesForWindowSize() {
-        const minWidth = 400;
-        const pageLayoutRect = pageLayout.getBoundingClientRect();
-        const totalWidth = pageLayoutRect.width;
-        let leftWidth, rightWidth;
+        // this function is called only if the window is big enough to show both panels
+        smallView = false;
+        leftSidePaddingLayout.style.gridTemplateRows = `auto`;
+        console.log('updateSidesForWindowSize');
         
+        const pageLayoutRect = pageLayout.getBoundingClientRect();
+        const totalWidth = pageLayoutRect.width; // total width of the page layout
+        // show everything
         divider.style.display = 'block';
         leftPanel.style.display = 'block';
         rightPanel.style.display = 'block';
-
-        if (totalWidth < 800) {
-            console.log("WRONG");
-            document.getElementById('left-partial').style.width = '100%';
-            document.getElementById('right-partial').style.width = '100%';
-            isDragging = false;
-        } else {
-            leftWidth = totalWidth * leftWidthRatio - dividerWidthHalf;
-            rightWidth = totalWidth - leftWidth - dividerWidthHalf;
-            
-            if (leftWidth === 0 || rightWidth === 0) {
-                leftWidth = totalWidth / 2 - dividerWidthHalf;
-                rightWidth = totalWidth / 2 - dividerWidthHalf;
-                leftWidthRatio = 0.5;
-                pageLayout.style.gridTemplateColumns = `${leftWidth}px ${dividerWidth}px ${rightWidth}px`;
-            }
-
-            if (leftWidth < minWidth) {
-                leftWidth = minWidth - dividerWidthHalf;
-                rightWidth = totalWidth - minWidth - dividerWidthHalf;
-                leftWidthRatio = leftWidth / totalWidth;
-            } else if (rightWidth < minWidth) {
-                rightWidth = minWidth - dividerWidthHalf;
-                leftWidth = totalWidth - minWidth - dividerWidthHalf;
-                leftWidthRatio = leftWidth / totalWidth;
-            }
-
-            document.getElementById('left-partial').style.width = `${leftWidth}px`;
-            document.getElementById('right-partial').style.width = `${rightWidth}px`;
-
-            divider.addEventListener('mousedown', function(e) {
-                e.preventDefault();
-                isDragging = true;
-            });
-
-            document.addEventListener('mousemove', function(e) {
-                if (!isDragging || totalWidth < 800) return;
-
-                leftWidth = e.clientX - pageLayoutRect.left;
-                rightWidth = pageLayoutRect.width - leftWidth - dividerWidthHalf;
-                leftWidthRatio = leftWidth / totalWidth;
-
-                if (leftWidth < minWidth) {
-                    leftWidth = minWidth - dividerWidthHalf;
-                    rightWidth = totalWidth - minWidth - dividerWidthHalf;
-                } else if (rightWidth < minWidth) {
-                    rightWidth = minWidth - dividerWidthHalf;
-                    leftWidth = totalWidth - minWidth - dividerWidthHalf;
-                }
-
-                leftWidthRatio = leftWidth / totalWidth;
-
-                document.getElementById('left-partial').style.width = `${leftWidth}px`;
-                document.getElementById('right-partial').style.width = `${rightWidth}px`;
-                document.documentElement.style.setProperty('--right-side-dependend', `${rightWidth * rightDependentRatio}px`);
-                console.log("setting right side dependend to " + rightWidth);
-            });
+        
+        if (leftWidthRatio === 0 || leftWidthRatio === 1 ||
+            totalWidth * leftWidthRatio < minWidth || totalWidth * (1 - leftWidthRatio) < minWidth) {
+            leftWidthRatio = 0.5;
         }
-
+        
+        let leftWidth = totalWidth * leftWidthRatio - dividerWidthHalf;
+        let rightWidth = totalWidth - leftWidth - dividerWidthHalf;
+        pageLayout.style.gridTemplateColumns = `${leftWidth}px ${dividerWidth}px ${rightWidth}px`;
+        
+        divider.addEventListener('mousedown', function(e) {
+            e.preventDefault();
+            isDragging = true;
+        });
+        
+        document.addEventListener('mousemove', function(e) {
+            if (!isDragging) return;
+            
+            leftWidth = e.clientX - pageLayoutRect.left;
+            rightWidth = pageLayoutRect.width - leftWidth - dividerWidthHalf;
+            leftWidthRatio = leftWidth / totalWidth;
+            
+            if (leftWidth < minWidth) {
+                leftWidth = minWidth;
+                rightWidth = totalWidth - minWidth - dividerWidthHalf;
+            } else if (rightWidth < minWidth) {
+                rightWidth = minWidth;
+                leftWidth = totalWidth - minWidth - dividerWidthHalf;
+            }
+            
+            leftWidthRatio = leftWidth / totalWidth;
+            
+            pageLayout.style.gridTemplateColumns = `${leftWidth}px ${dividerWidth}px ${rightWidth}px`;
+            document.documentElement.style.setProperty('--right-side-dependend', `${rightWidth * rightDependentRatio}px`);
+        });
+        
+        divider.addEventListener('mouseup', function() {
+            isDragging = false;
+        });
+        
         leftWidth = totalWidth * leftWidthRatio;
         rightWidth = totalWidth - leftWidth - dividerWidthHalf;
         document.documentElement.style.setProperty('--right-side-dependend', `${rightWidth * rightDependentRatio}px`);
+        pageLayout.style.gridTemplateColumns = `${leftWidth}px ${dividerWidth}px ${rightWidth}px`;
     }
-
-    //
-    // window.addEventListener('resize', updateSidesForWindowSize);
 
     document.addEventListener('mouseup', function() {
         isDragging = false;
@@ -102,18 +87,25 @@ document.addEventListener('DOMContentLoaded', function() {
         if (window.innerWidth <= 800) {
             const pageLayoutRect = pageLayout.getBoundingClientRect();
             const totalWidth = pageLayoutRect.width;
+            if (!smallView) {
+                smallView = true;
+                leftSidePaddingLayout.style.gridTemplateRows = `auto 7vh`;
+                console.log('updatePanelVisibility1');
+            }
             if (isLeftPanelVisible) {
-                pageLayout.style.gridTemplateColumns = `${totalWidth}px 0% 0%`;
+                pageLayout.style.gridTemplateColumns = `100%`;
                 rightPanel.style.display = 'none';
                 leftPanel.style.display = 'block';
                 divider.style.display = 'none';
+                console.log('updatePanelVisibility2');
             } else {
-                pageLayout.style.gridTemplateColumns = `0% 0% ${totalWidth}px`;
+                pageLayout.style.gridTemplateColumns = `100%`;
                 rightPanel.style.display = 'block';
                 leftPanel.style.display = 'none';
                 divider.style.display = 'none';
+                console.log('updatePanelVisibility3');
             }
-            
+            document.documentElement.style.setProperty('--right-side-dependend', `${totalWidth * rightDependentRatio}px`);
         } else {
             updateSidesForWindowSize();
         }
