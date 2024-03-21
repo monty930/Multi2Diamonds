ef31a4b6d7406b61e2d3d014d51a84cc4a2465df
Krystyna Gasinska <krystyna.gas01@gmail.com>
2024-03-21 16:05:29 +0100

    db profile

--- Migrations/20240321150403_UsersSavedContentTable.Designer.cs
+++ Migrations/20240321150403_UsersSavedContentTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BridgeScenarios.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BridgeScenarios.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240321150403_UsersSavedContentTable")]
+    partial class UsersSavedContentTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
