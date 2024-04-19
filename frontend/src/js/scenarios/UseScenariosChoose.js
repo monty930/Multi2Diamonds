d23b67bd31eefbe0829e55421e8882c04963d883
Krystyna Gasinska <krystyna.gas01@gmail.com>
2024-04-19 22:31:45 +0200

    settings css

--- frontend/src/js/scenarios/UseScenariosChoose.js
+++ frontend/src/js/scenarios/UseScenariosChoose.js
@@ -3,15 +3,69 @@ import { useScenario } from './CompilerSettings';
 import { useNavigate } from 'react-router-dom';
 import '../../css/Scenarios/UseScenariosChoose.css';
 import arrowDown from '../../assets/arrow-down.png';
+import arrowUp from '../../assets/arrow-up.png';
 
 function UseScenariosChoose() {
     const { setVul, setDealer, setNumberOfDeals } = useScenario();
     const navigate = useNavigate();
+    const [constraints, setConstraints] = useState([]);
+
+    const addConstraintForDealSet = () => {
+        setConstraints(prev => [
+            ...prev,
+            {
+                id: prev.length,
+                value: "Random"
+            }
+        ]);
+    }
+
+    const handleConstraintChange = (id, value) => {
+        setConstraints(prev => prev.map(c => c.id === id ? { ...c, value: value } : c));
+    }
 
     return (
         <div className={"ScenariosLeftOuterContainer"}>
             <div className="LeftSideTitleContainer">
                 <span className="LeftSideTitle">Choose scenarios for a deal set:</span>
+            </div>
+            <div className="SettingsPadding">
+                <div className={"SettingsContainer"}>
+                    <div className={"OneSettingRow"}>
+                        <div className={"SettingsChoice ConstraintSelect"}>
+                            <div className={"CustomSelectContainer"}>
+                                <select
+                                    className={"SettingsSelect"}
+                                    onChange={(e) => setVul(e.target.value)}
+                                    defaultValue="Random"
+                                    id="VulSelect">
+                                    <option value="Random">Random</option>
+                                    <option value="None">None</option>
+                                    <option value="All">All</option>
+                                    <option value="NS">NS</option>
+                                    <option value="EW">EW</option>
+                                    <option value="Matching">Matching numbers</option>
+                                </select>
+                                <div className={"CustomSelectArrow"}>
+                                    <img src={arrowDown} alt={"arrow-down"} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"SettingsBreak"}></div>
+                    <div className={"OneSettingRow"}>
+                        <div className={"AddConstraintRow"}>
+                            <button
+                                className={"AnyButton AddConstraintButton"}
+                                onClick={addConstraintForDealSet}>
+                                Add constraint
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="LeftSideTitleContainer">
+                <span className="LeftSideTitle">Deal set setting:</span>
             </div>
             <div className="SettingsPadding">
                 <div className={"SettingsContainer"}>
@@ -69,29 +123,29 @@ function UseScenariosChoose() {
                         <div className="SettingsTitle">
                             Number of deals:
                         </div>
-                        <div className={"InputNumberSetting"}>
+                        <div className={"NumberSpinner"}>
                             <input
+                                className={"InputNumberSetting"}
                                 type="number"
                                 defaultValue={12}
                                 min={1}
                                 onChange={(e) => setNumberOfDeals(parseInt(e.target.value, 10) || 1)}
-                                className={"SettingsNumberInput"}
                             />
+                            <div className={"InputNumberArrows"}>
+                                <button 
+                                    type={"button"}
+                                    className={"NumberSpinnerButton ButtonIncr"}>
+                                    <img src={arrowUp} alt={"increase"} />
+                                </button>
+                                <button 
+                                    type={"button"}
+                                    className={"NumberSpinnerButton ButtonDecr"}>
+                                    <img src={arrowDown} alt={"decrease"} />
+                                </button>
+                            </div>
                         </div>
                     </div>
-                    
-                    
-                    {/*<div>dealer: </div>*/}
-                    {/*<select onChange={(e) => setDealer(e.target.value)} defaultValue="Random">*/}
-                    {/*    <option value="North">North</option>*/}
-                    {/*    <option value="South">South</option>*/}
-                    {/*    <option value="West">West</option>*/}
-                    {/*    <option value="East">East</option>*/}
-                    {/*    <option value="Random">Random</option>*/}
-                    {/*    <option value="Matching">Matching numbers</option>*/}
-                    {/*</select>*/}
-                    {/*<div>numberOfDeals: </div>*/}
-                    {/*<input type="number" defaultValue={12} min={1} onChange={(e) => setNumberOfDeals(parseInt(e.target.value, 10) || 1)} />*/}
+                    <div className={"SettingsBreak"}></div>
                 </div>
                 <div className="ToggleScenariosViewButtonContainer">
                     <button
