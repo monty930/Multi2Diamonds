7083631f200c7a9c1846b0cf8b754c2c1338e3d8
Krystyna Gasinska <krystyna.gas01@gmail.com>
2024-04-22 19:45:54 +0200

    Error page

--- frontend/src/js/WelcomePage.js
+++ frontend/src/js/WelcomePage.js
@@ -3,10 +3,7 @@ import React from "react";
 function WelcomePage() {
     return (
         <div className="WelcomePage">
-            <h1>Welcome to the 2DIAMS Scenario Builder!</h1>
-            <p>
-                This tool allows you to create and use scenarios for the 2DIAMS project.
-            </p>
+            Welcome page
         </div>
     );
 }
